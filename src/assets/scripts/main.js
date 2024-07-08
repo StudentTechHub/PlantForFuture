b11096bb45c3a962b1ca0e0615df17f667b2a9b7
Neeraj <neerajantil.antil26@gmail.com>
2024-07-08 13:51:37 +0530

    Update Style Configuration, Left Nav Responsiveness
    Add Tailwind CSS and update code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,10 @@
 // Use fetch to include components dynamically
-const components = ["navbar/navbar"];
+// const components = [""];
 
-components.forEach((component) => {
-  fetch(`/src/components/${component}.html`)
-    .then((response) => response.text())
-    .then((html) => {
-      document.querySelector("#app").innerHTML += html;
-    });
-});
+// components.forEach((component) => {
+//   fetch(`/src/components/${component}.html`)
+//     .then((response) => response.text())
+//     .then((html) => {
+//       document.querySelector("#app").innerHTML += html;
+//     });
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"
+  ],
   theme: {
     extend: {
       backdropBlur: {
@@ -8,26 +12,26 @@ module.exports = {
         gigant: "180px",
       },
       colors: {
-        'primary': {
-          default: '#339dff',
-          hover: '#0084ff',
-          'red': {
-            default: '#ec8273',
-            hover: '#e55742'
-          }
+        primary: {
+          default: "#339dff",
+          hover: "#0084ff",
+          red: {
+            default: "#ec8273",
+            hover: "#e55742",
+          },
         },
-        'light': '#f1f6fb',
-        'blackPearl': '#0b1825',
-        'darkBlue': '#112337',
-        'darkGray': 'rgba(246,241,251,5%)',
-        'lightGray': '#808c98',
-        'extraLightGray': '#8c97a3',
+        light: "#f1f6fb",
+        blackPearl: "#0b1825",
+        darkBlue: "#112337",
+        darkGray: "rgba(246,241,251,5%)",
+        lightGray: "#808c98",
+        extraLightGray: "#8c97a3",
       },
       fontFamily: {
-        'manrope': ['Manrope', 'sans-serif'],
-        'quicksand': ['Quicksand', 'sans-serif'],
-        'poppins': ['Poppins', 'sans-serif'],
-        'montserrat': ['Montserrat', 'sans-serif']
+        manrope: ["Manrope", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
       fontSize: {
         xxs: ["10px", {}],
@@ -35,4 +39,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
