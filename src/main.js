78e9e7f096ec0db1f9d71e96467640eaa3f5afee
Neeraj <neerajantil.antil26@gmail.com>
2024-07-09 13:16:44 +0530

    Fix Nav JS

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", () => {
-  const isMenuOpen = mobileMenu.classList.contains("-translate-x-full");
-
-  // hamburger.querySelector("img").src = isMenuOpen
-  //   ? "/assets/images/svg/hamburger.svg"
-  //   : "/assets/images/svg/close.svg";
-
   mobileMenu.classList.toggle("-translate-x-full");
   mobileMenu.classList.toggle("-translate-x-0");
 });
