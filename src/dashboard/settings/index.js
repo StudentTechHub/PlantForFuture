a2f191169f310f91afd319b44cf7122d13549080
Neeraj <neerajantil.antil26@gmail.com>
2024-07-13 22:47:09 +0530

    make settings + responsive

--- src/dashboard/settings/index.js
+++ src/dashboard/settings/index.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const userInfoDiv = document.querySelector("#user-info");
   const avatarDiv = document.querySelector("#avatar-dropdown");
   const dropdownMenu = document.querySelector("#dropdown-menu");
 
+  const visibility = document.querySelectorAll(".eye-button");
+  const password = document.querySelectorAll("input[type='password']");
+
+  // Handle Password Visibility
+  visibility.forEach((visibilityButton, index) => {
+    visibilityButton.addEventListener("click", () => {
+      const type =
+        password[index].getAttribute("type") === "password"
+          ? "text"
+          : "password";
+      visibilityButton.src =
+        type === "password"
+          ? "/assets/images/svg/visibility_off.svg"
+          : "/assets/images/svg/visibility.svg";
+      password[index].setAttribute("type", type);
+    });
+  });
+
   const fetchUserData = async () => {
     try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/v1/volunteer/me`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch(`/api/v1/volunteer/me`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -28,11 +42,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   };
 
   const displayUserData = (data) => {
-    userInfoDiv.innerHTML = `
-          <p><strong>Name:</strong> ${data.fullName}</p>
-          <p><strong>Email:</strong> ${data.email}</p>
-      `;
-
     document.querySelector(".pfp").src = `/assets/images/volunteer-${
       data.gender === "male" ? "boy" : "girl"
     }.png`;
@@ -60,15 +69,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Logout
   document.querySelector("#logout").addEventListener("click", async () => {
     try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/v1/volunteer/logout`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch(`/api/v1/volunteer/logout`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
