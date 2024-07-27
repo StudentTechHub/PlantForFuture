1cad73a0214e31c6167123b1c8a1729f53e6b2e1
Neeraj <neerajantil.antil26@gmail.com>
2024-07-27 13:27:13 +0530

    enable redirects

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,24 +1,24 @@
 // Authenticate Dashboard
 
-// if (document.cookie !== null && document.cookie !== undefined) {
-//   const cookies = document.cookie.split(";");
-//   const cookieObj = cookies.reduce((acc, cookie) => {
-//     const [key, value] = cookie.split("=");
-//     acc[key.trim()] = value;
-//     return acc;
-//   }, {});
+if (document.cookie !== null && document.cookie !== undefined) {
+  const cookies = document.cookie.split(";");
+  const cookieObj = cookies.reduce((acc, cookie) => {
+    const [key, value] = cookie.split("=");
+    acc[key.trim()] = value;
+    return acc;
+  }, {});
 
-//   if (!cookieObj["_volunteer_token"] && !cookieObj["_creator_token"]) {
-//     window.location.href = "/join-us/";
-//   }
+  if (!cookieObj["_volunteer_token"] && !cookieObj["_creator_token"]) {
+    window.location.href = "/join-us/";
+  }
 
   // if (cookieObj["_creator_token"]) {
-  //   window.location.href = "/src/dashboard/creatorDashboard/";
+  //   window.location.href = "/src/dashboard/creator/";
   //   return;
   // }
 
   // if (cookieObj["_volunteer_token"]) {
-  //   window.location.href = "/src/dashboard/volunteerDashboard/";
+  //   window.location.href = "/src/dashboard/volunteer/";
   //   return;
   // }
-// }
+}
